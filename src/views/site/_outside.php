16761d99e068232db43adeae3b55045f3fee2651
Szántó Zoltán <mrbig00@gmail.com>
2018-07-23 23:21:30 +0300

    Fix index/_outside & index/_room messup

---
+++ src/views/site/_outside.php
@@ -0,0 +1,84 @@
+<?php
+/**
+ * @author  Zoltan Szanto <mrbig00@gmail.com>
+ * @since   2018/07/23
+ *
+ * @var $this        yii\web\View
+ * @var $measurement \app\models\Measurement
+ */
+
+use rmrevin\yii\fontawesome\FA;
+?>
+<div class="row">
+    <div class="col-md-3 col-sm-6 col-xs-12">
+        <div class="info-box bg-blue">
+            <span class="info-box-icon">
+                <?= FA::i(FA::_THERMOMETER) ?>
+            </span>
+
+            <div class="info-box-content">
+                <span class="info-box-text"><?= \Yii::t('app', 'Outside temperature') ?></span>
+                <span class="info-box-number">
+                    <?= $measurement->outside_temperature ?>
+                    <sup>
+                        &#8451;
+                    </sup>
+                </span>
+            </div>
+        </div>
+    </div>
+
+    <div class="col-md-3 col-sm-6 col-xs-12">
+        <div class="info-box bg-aqua">
+            <span class="info-box-icon">
+                <?= FA::i(FA::_TINT) ?>
+            </span>
+
+            <div class="info-box-content">
+                <span class="info-box-text"><?= \Yii::t('app', 'Outside humidity') ?></span>
+                <span class="info-box-number">
+                    <?= $measurement->outside_humidity ?>
+                    <sup>
+                        %
+                    </sup>
+                </span>
+            </div>
+        </div>
+    </div>
+
+    <div class="col-md-3 col-sm-6 col-xs-12">
+        <div class="info-box bg-teal">
+            <span class="info-box-icon">
+                <?= FA::i(FA::_TACHOMETER) ?>
+            </span>
+
+            <div class="info-box-content">
+                <span class="info-box-text"><?= \Yii::t('app', 'Outside air pressure') ?></span>
+                <span class="info-box-number">
+                    <?= $measurement->outside_air_pressure ?>
+                    <sup>
+                        mmHg
+                    </sup>
+                </span>
+            </div>
+        </div>
+    </div>
+
+    <div class="col-md-3 col-sm-6 col-xs-12">
+        <div class="info-box bg-orange">
+            <span class="info-box-icon">
+                <?= FA::i(FA::_LEAF) ?>
+            </span>
+
+            <div class="info-box-content">
+                <span class="info-box-text"><?= \Yii::t('app', 'Outside wind speed') ?></span>
+                <span class="info-box-number">
+                    <?= $measurement->outside_wind_speed ?>
+                    <sup>
+                        km/h
+                    </sup>
+                </span>
+            </div>
+        </div>
+    </div>
+</div>
---
+++ src/views/site/_room.php
@@ -0,0 +1,84 @@
+<?php
+/**
+ * @author  Zoltan Szanto <mrbig00@gmail.com>
+ * @since   2018/07/23
+ *
+ * @var $this        yii\web\View
+ * @var $measurement \app\models\Measurement
+ */
+
+use rmrevin\yii\fontawesome\FA;
+?>
+<div class="row">
+    <div class="col-md-3 col-sm-6 col-xs-12">
+        <div class="info-box">
+            <span class="info-box-icon bg-blue">
+                <?= FA::i(FA::_THERMOMETER) ?>
+            </span>
+
+            <div class="info-box-content">
+                <span class="info-box-text"><?= \Yii::t('app', 'Room temperature') ?></span>
+                <span class="info-box-number">
+                    <?= $measurement->room_temperature ?>
+                    <sup>
+                        &#8451;
+                    </sup>
+                </span>
+            </div>
+        </div>
+    </div>
+
+    <div class="col-md-3 col-sm-6 col-xs-12">
+        <div class="info-box">
+            <span class="info-box-icon bg-aqua">
+                <?= FA::i(FA::_TINT) ?>
+            </span>
+
+            <div class="info-box-content">
+                <span class="info-box-text"><?= \Yii::t('app', 'Room humidity') ?></span>
+                <span class="info-box-number">
+                    <?= $measurement->room_humidity ?>
+                    <sup>
+                        %
+                    </sup>
+                </span>
+            </div>
+        </div>
+    </div>
+
+    <div class="col-md-3 col-sm-6 col-xs-12">
+        <div class="info-box">
+            <span class="info-box-icon bg-teal">
+                <?= FA::i(FA::_TACHOMETER) ?>
+            </span>
+
+            <div class="info-box-content">
+                <span class="info-box-text"><?= \Yii::t('app', 'Room air pressure') ?></span>
+                <span class="info-box-number">
+                    <?= $measurement->room_air_pressure ?>
+                    <sup>
+                        mmHg
+                    </sup>
+                </span>
+            </div>
+        </div>
+    </div>
+
+    <div class="col-md-3 col-sm-6 col-xs-12">
+        <div class="info-box">
+            <span class="info-box-icon bg-yellow-active">
+                <?= FA::i(FA::_LIGHTBULB_O) ?>
+            </span>
+
+            <div class="info-box-content">
+                <span class="info-box-text"><?= \Yii::t('app', 'Room illuminance') ?></span>
+                <span class="info-box-number">
+                    <?= $measurement->room_lux ?>
+                    <sup>
+                        lux
+                    </sup>
+                </span>
+            </div>
+        </div>
+    </div>
+</div>
